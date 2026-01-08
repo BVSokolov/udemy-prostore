c94c896f7ff5cc91bfffc7add37a8304fc989b26
Bran <sokolov.branimir@gmail.com>
2026-01-08 17:40:24 +0100

    now connecting to db using neon adapter

--- db/prisma-client.ts
+++ db/prisma-client.ts
@@ -1,6 +1,31 @@
 import "dotenv/config"
 import { PrismaPg } from "@prisma/adapter-pg"
 import { PrismaClient } from "@/lib/generated/prisma/client"
+// import { neonConfig, Pool } from "@neondatabase/serverless"
+import { PrismaNeon } from "@prisma/adapter-neon"
+// import ws from "ws"
+
+// neonConfig.webSocketConstructor = ws
+const connectionString = `${process.env.DATABASE_URL}`
+
+// const pool = new Pool({ connectionString })
+const adapter = new PrismaNeon({ connectionString })
+export const prisma = new PrismaClient({ adapter }).$extends({
+  result: {
+    product: {
+      price: {
+        compute(product) {
+          return product.price.toString()
+        },
+      },
+      rating: {
+        compute(product) {
+          return product.rating.toString()
+        },
+      },
+    },
+  },
+})
 
 export default function getClient() {
   const connectionString = `${process.env.DATABASE_URL}`
