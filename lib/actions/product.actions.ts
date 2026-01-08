9314ab3ca17bc26e2d040165a2d8d27136736c4a
Bran <sokolov.branimir@gmail.com>
2026-01-08 18:14:10 +0100

    added product page, added product query by slug

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -2,6 +2,7 @@
 import { prisma } from "@/db/prisma-client"
 import { convertToPlainObject } from "../utils"
 import { LATEST_PRODUCTS_LIMIT } from "../constants"
+import { Product } from "@/types"
 
 // Get latest products
 export async function getLatestProducts() {
@@ -10,4 +11,11 @@ export async function getLatestProducts() {
     orderBy: { createdAt: "desc" },
   })
   return convertToPlainObject(data)
+}
+
+// Get single product by it's slug
+export async function getProductBySlug(slug: Product["slug"]) {
+  return await prisma.product.findFirst({
+    where: { slug },
+  })
 }
