d21427cd707d76da67cbf4a8f128ecf25f843e60
Bran <sokolov.branimir@gmail.com>
2026-01-14 15:39:51 +0100

    added cart page, cart table and attempted a fix on the middleware file to see if it will deploy on vercel

---
+++ app/(root)/cart/page.tsx
@@ -0,0 +1,18 @@
+import { getMyCart } from "@/lib/actions/cart.actions"
+import CartTable from "./cart-table"
+
+export const metadata = {
+  title: "Shoppng Cart",
+}
+
+const CartPage = async () => {
+  const cart = await getMyCart()
+
+  return (
+    <>
+      <CartTable cart={cart} />
+    </>
+  )
+}
+
+export default CartPage
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,6 @@
 import NextAuth from "next-auth"
 import { authConfig } from "./auth.config"
 
-export const { auth: middleware } = NextAuth(authConfig)
+// TODO: The "middleware" file convention is deprecated. Please use "proxy" instead. Learn more: https://nextjs.org/docs/messages/middleware-to-proxy
+const { auth: middleware } = NextAuth(authConfig)
+export default middleware
