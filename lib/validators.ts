8ae3e03570ed81cbd66d530668189e7996fa5103
Bran <sokolov.branimir@gmail.com>
2026-01-15 16:24:20 +0100

    added payment method action and schema

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,5 +1,6 @@
 import z from "zod"
 import { formatNumberWithDecimal } from "./utils"
+import { PAYMENT_METHODS } from "./constants"
 
 const CURRENCY_SCHEMA_VALIDATOR = z
   .string()
@@ -87,3 +88,13 @@ export const shippingAddressSchema = z.object({
   lat: z.number().optional(),
   lng: z.number().optional(),
 })
+
+// Schema for payment method
+export const paymentMethodSchema = z
+  .object({
+    type: z.string().min(1, "Payment method is required"),
+  })
+  .refine((data) => PAYMENT_METHODS.includes(data.type), {
+    path: ["type"],
+    error: "Invalid payment method",
+  })
