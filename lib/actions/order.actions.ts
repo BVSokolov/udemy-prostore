3d36ff5d8991435f748beb8eba410782756efdc9
Bran <sokolov.branimir@gmail.com>
2026-01-16 16:42:14 +0100

    added place order action and form and wip order details page

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,7 +1,7 @@
 "use server"
 
 import { isRedirectError } from "next/dist/client/components/redirect-error"
-import { formatError } from "../utils"
+import { convertToPlainObject, formatError } from "../utils"
 import { getMyCart } from "./cart.actions"
 import { auth } from "@/auth"
 import { getUserById } from "./user.actions"
@@ -95,4 +95,19 @@ export async function createOrder() {
     if (isRedirectError(error)) throw error
     return { success: false, message: formatError(error) }
   }
+}
+
+// Get order by id
+export async function getOrderById(orderId: string) {
+  const data = await prisma.order.findFirst({
+    where: {
+      id: orderId,
+    },
+    include: {
+      orderitems: true,
+      user: { select: { name: true, email: true } },
+    },
+  })
+
+  return convertToPlainObject(data)
 }
