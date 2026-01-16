37ce30f266aab33be74f9f83f30b8e25b7dbb2a0
Bran <sokolov.branimir@gmail.com>
2026-01-16 17:18:11 +0100

    added formatdatetime util, order details page and table WITH EVEN MORE CODE DUPLICATION AAAAHHHHH

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next"
 import { getOrderById } from "@/lib/actions/order.actions"
 import { notFound } from "next/navigation"
 import { ShippingAddress } from "@/types"
+import OrderDetailsTable from "./order-details-table"
 
 export const metadata: Metadata = {
   title: "Order Details",
@@ -13,7 +14,14 @@ const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
 
   if (!order) notFound()
 
-  return <>Details</>
+  return (
+    <OrderDetailsTable
+      order={{
+        ...order,
+        shippingAddress: order.shippingAddress as ShippingAddress,
+      }}
+    />
+  )
 }
 
 export default OrderDetailsPage
