099df2c15e6090aa80881488bfe6f1afbe69913e
Bran <sokolov.branimir@gmail.com>
2026-02-12 17:03:49 +0100

    added review table

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -53,3 +53,9 @@ export const productDefaultValues = {
 export const USER_ROLES = process.env.USER_ROLES
   ? process.env.USER_ROLES.split(", ")
   : ["admin", "user"]
+
+export const REVIEW_FORM_DEFAULT_VALUES = {
+  title: "",
+  comment: "",
+  rating: 0,
+}
